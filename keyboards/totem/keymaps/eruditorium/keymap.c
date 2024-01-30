7c1ac65ecfa7237b31dac1571a2ed16f0953f5d6
Nico <nico@iceberg.local>
2024-01-30 20:38:02 +0100

    debugging totem

--- keyboards/totem/keymaps/eruditorium/keymap.c
+++ keyboards/totem/keymaps/eruditorium/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
               KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
               KC_A,     KC_S,     KC_D,     SHT_F,    KC_G,      KC_H,     SHT_J,    KC_K,     KC_L,     KC_SCLN,
-    CTL_MINS, CTL_Z,    GUI_X,    ALT_C,    KC_V,     KC_B,      KC_N,     KC_M,     ALT_COMM, GUI_DOT,  CTL_SLSH,  MC_QUOT,
+    CTL_MNS,  CTL_Z,    GUI_X,    ALT_C,    KC_V,     KC_B,      KC_N,     KC_M,     ALT_COMM, GUI_DOT,  CTL_SLSH,  MC_QUOT,
                                   KC_BSPC,  LOWER,    KC_ENTER,  KC_SPACE, RAISE,    ALT_DEL
     ),
 /*
