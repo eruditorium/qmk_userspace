8061d10cdb334385a22663240b9f7ea90ca2d3ec
Nico <60152863+eruditorium@users.noreply.github.com>
2024-01-31 08:18:12 +0100

    Update keymap.c
    another typo

--- keyboards/totem/keymaps/eruditorium/keymap.c
+++ keyboards/totem/keymaps/eruditorium/keymap.c
@@ -224,7 +224,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-#include "feature/tapping_term.c"
+#include "features/tapping_term.c"
 /*
   ╺━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╸
 
