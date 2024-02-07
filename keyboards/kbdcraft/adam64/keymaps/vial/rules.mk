2c868e51526155d5ad6611d4446a40654c7f0dc5
eruditorium <y.t@gmx.de>
2024-02-07 22:17:52 +0100

    added vial firmware

--- keyboards/kbdcraft/adam64/keymaps/vial/rules.mk
+++ keyboards/kbdcraft/adam64/keymaps/vial/rules.mk
@@ -1,2 +1,15 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
+
+LTO_ENABLE = yes
+
+#VIAL_INSECURE = yes # ~374 bytes.
+
+QMK_SETTINGS = yes # ~2560 bytes
+TAP_DANCE_ENABLE = yes # ~2196 bytes
+COMBO_ENABLE = yes # ~2408 bytes
+
+SPACE_CADET_ENABLE = no # ~324 bytes
+MAGIC_ENABLE = no # ~584 bytes
+KEY_OVERRIDE_ENABLE = no
