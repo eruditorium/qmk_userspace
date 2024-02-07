56b7348c99a0b83bc6eedcfda4af6d5c0d8561f3
Nico <60152863+eruditorium@users.noreply.github.com>
2024-02-07 13:16:25 +0100

    defined timeout for autoshift

--- keyboards/crkbd/keymaps/eruditorium/config.h
+++ keyboards/crkbd/keymaps/eruditorium/config.h
@@ -51,7 +51,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Auto Shift
 // #define NO_AUTO_SHIFT_ALPHA
-// #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
 // #define AUTO_SHIFT_NO_SETUP
 
 #undef LOCKING_SUPPORT_ENABLE
