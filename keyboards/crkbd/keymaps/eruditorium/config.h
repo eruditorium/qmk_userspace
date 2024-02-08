e632dfc38afdefd6b052b1196d2155d9d0008fa6
Nico <60152863+eruditorium@users.noreply.github.com>
2024-02-08 09:32:21 +0100

    Update config.h

--- keyboards/crkbd/keymaps/eruditorium/config.h
+++ keyboards/crkbd/keymaps/eruditorium/config.h
@@ -169,5 +169,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 #define NO_ACTION_ONESHOT
+#define DISABLE_LEADER
+
 
 
