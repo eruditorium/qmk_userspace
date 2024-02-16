e32bf1919d12a60be46df0658b39e2577d7183f7
Nicolas Trapp <n.trapp@du.szmf.de>
2024-02-16 19:11:27 +0100

    reducing size of corne keymap

--- keyboards/crkbd/keymaps/eruditorium/config.h
+++ keyboards/crkbd/keymaps/eruditorium/config.h
@@ -51,9 +51,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Auto Shift
 // #define NO_AUTO_SHIFT_ALPHA
-//define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
 // #define AUTO_SHIFT_NO_SETUP
-//#define AUTO_SHIFT_MODIFIERS
+#define AUTO_SHIFT_MODIFIERS
 
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
@@ -63,9 +63,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
 // #define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger
 
-
 #ifdef RGBLIGHT_ENABLE
-    //#undef RGBLED_NUM
+    #undef RGBLED_NUM
     #undef RGBLIGHT_EFFECT_BREATHING
     #undef RGBLIGHT_EFFECT_RAINBOW_MOOD
     #undef RGBLIGHT_EFFECT_KNIGHT
@@ -81,65 +80,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
-#endif
-
-#ifdef RGB_MATRIX_ENABLE
-    #undef RGB_MATRIX_KEYPRESSES // reacts to keypresses
-    #undef RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
-    #undef RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-    #undef RGB_MATRIX_FRAMEBUFFER_EFFECTS
-    // #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-    // #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-    #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
-    #define RGB_MATRIX_HUE_STEP 4
-    #define RGB_MATRIX_SAT_STEP 4
-    #define RGB_MATRIX_VAL_STEP 4
-    #define RGB_MATRIX_SPD_STEP 10
-
-    /* Disable the animations you don&#39;t want/need.  You will need to disable a good number of these    *
-     * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
-    #undef ENABLE_RGB_MATRIX_ALPHAS_MODS
-    #undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-    #undef ENABLE_RGB_MATRIX_BREATHING
-    #undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-    #undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
-    #undef ENABLE_RGB_MATRIX_BAND_SAT
-    #undef ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
-    #undef ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
-    #undef ENABLE_RGB_MATRIX_BAND_VAL
-    #undef ENABLE_RGB_MATRIX_CYCLE_ALL
-    #undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-    #undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
-    #undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
-    #undef ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
-    #undef ENABLE_RGB_MATRIX_CYCLE_SPIRAL
-    #undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
-    #undef ENABLE_RGB_MATRIX_DUAL_BEACON
-    #undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-    #undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-    #undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-    #undef ENABLE_RGB_MATRIX_SOLID_SPLASH
-    #undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-    #undef ENABLE_RGB_MATRIX_SOLID_SPLASH
-    #undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-    #undef ENABLE_RGB_MATRIX_SPLASH
-    #undef ENABLE_RGB_MATRIX_MULTISPLASH
-    #undef ENABLE_RGB_MATRIX_SPLASH
-    #undef ENABLE_RGB_MATRIX_MULTISPLASH
 #endif
 
 //#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
