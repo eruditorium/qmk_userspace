e008e68029e25ab88cc80776083aa41219281085
Nico <60152863+eruditorium@users.noreply.github.com>
2024-02-25 16:56:21 +0100

    reduced combos

--- features/combos.c
+++ features/combos.c
@@ -8,15 +8,6 @@
 enum combos {
   QA_ESC,
 
-  ED_HOME,
-  DC_END,
-
-  SD_WRDLFT,
-  DF_WRDRGHT,
-
-  IK_PGUP,
-  KCOMMA_PGDWN,
-
   RT_LEFTBRACE,
   FG_LEFTBRACKET,
   VB_LEFTPARENTHESIS,
@@ -32,15 +23,6 @@ enum combos {
 };
 
 const uint16_t PROGMEM qa_combo[] = {KC_Q, KC_A, COMBO_END};
-
-const uint16_t PROGMEM ed_combo[] = {KC_E, KC_D, COMBO_END};
-const uint16_t PROGMEM dc_combo[] = {KC_D, ALT_C, COMBO_END};
-
-const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM df_combo[] = {KC_D, SHT_F, COMBO_END};
-
-const uint16_t PROGMEM ik_combo[] = {KC_I, KC_K, COMBO_END};
-const uint16_t PROGMEM kcomma_combo[] = {KC_K, ALT_COMM, COMBO_END};
 
 const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
 const uint16_t PROGMEM fg_combo[] = {SHT_F, KC_G, COMBO_END};
@@ -59,15 +41,6 @@ const uint16_t PROGMEM jm_combo[] = {SHT_J, KC_M, COMBO_END};
 combo_t key_combos[] = {
   [QA_ESC] = COMBO(qa_combo, KC_ESC),
 
-  [SD_WRDLFT] = COMBO(sd_combo, LCTL_T(KC_LEFT)),
-  [DF_WRDRGHT] = COMBO(df_combo, LCTL_T(KC_RIGHT)),
-
-  [ED_HOME] = COMBO(ed_combo, KC_HOME),
-  [DC_END] = COMBO(dc_combo, KC_END),
-
-  [IK_PGUP] = COMBO(ik_combo, KC_PGUP),
-  [KCOMMA_PGDWN] = COMBO(kcomma_combo, KC_PGDN),
-
   [RT_LEFTBRACE] = COMBO(rt_combo, KC_LCBR),
   [FG_LEFTBRACKET] = COMBO(fg_combo, KC_LBRC),
   [VB_LEFTPARENTHESIS] = COMBO(vb_combo, KC_LPRN),
@@ -79,6 +52,6 @@ combo_t key_combos[] = {
   [GB_MINUS] = COMBO(gb_combo, KC_MINS),
   [HN_UNDERSCORE] = COMBO(hn_combo, KC_UNDS),
 
-  [FV_DOUBLEQUOTE] = COMBO(fv_combo, KC_QUOT),
-  [JM_QUOTE] = COMBO(jm_combo, KC_DQT),
+  [FV_DOUBLEQUOTE] = COMBO(fv_combo, KC_DQT),
+  [JM_QUOTE] = COMBO(jm_combo, KC_QUOT),
 };
