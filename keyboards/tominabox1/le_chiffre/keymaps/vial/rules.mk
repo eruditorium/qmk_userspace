888a7eaec6a1041d81fde9430bce7512c0916ba5
Nicolas Trapp <n.trapp@du.szmf.de>
2024-04-19 14:52:42 +0200

    tweaking memory vial

--- keyboards/tominabox1/le_chiffre/keymaps/vial/rules.mk
+++ keyboards/tominabox1/le_chiffre/keymaps/vial/rules.mk
@@ -1,5 +1,16 @@
 VIA_ENABLE = yes
 ENCODER_MAP_ENABLE = no
 VIAL_ENABLE = yes
-VIAL_INSECURE = yes
 VIALRGB_ENABLE = yes
+
+LTO_ENABLE = yes
+
+#VIAL_INSECURE = yes # ~374 bytes.
+
+QMK_SETTINGS = yes # ~2560 bytes
+TAP_DANCE_ENABLE = yes # ~2196 bytes
+COMBO_ENABLE = yes # ~2408 bytes
+
+SPACE_CADET_ENABLE = no # ~324 bytes
+MAGIC_ENABLE = no # ~584 bytes
+KEY_OVERRIDE_ENABLE = no
