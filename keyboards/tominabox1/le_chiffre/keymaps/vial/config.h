7f9162123623741e66ab36186dbd7d1a1c8fe6ef
Nicolas Trapp <n.trapp@du.szmf.de>
2024-04-19 15:05:16 +0200

    more debugging

--- keyboards/tominabox1/le_chiffre/keymaps/vial/config.h
+++ keyboards/tominabox1/le_chiffre/keymaps/vial/config.h
@@ -1,6 +1,5 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0xC9, 0xD4, 0x38, 0x79, 0x25, 0x61, 0x35, 0x00}
-#define VIAL_COMBO_ENTRIES 12
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 #define TAPPING_TERM 200
