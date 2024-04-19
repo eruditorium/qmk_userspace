74e3fb94ebbd8c8ad799b16f7312dab7923b7429
Nicolas Trapp <n.trapp@du.szmf.de>
2024-04-19 15:17:47 +0200

    debugging

--- keyboards/tominabox1/le_chiffre/keymaps/vial/keymap.c
+++ keyboards/tominabox1/le_chiffre/keymaps/vial/keymap.c
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include "../../rgb.c"
 
 #define HM_A	LSFT_T(KC_A)
 #define HM_S	LALT_T(KC_S)
@@ -38,13 +37,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        _______,  KC_ENT,             KC_TAB,  _______
     )
 };
-
-#ifdef ENCODER_MAP_ENABLE
-#undef BLANK_LAYER
-#define BLANK_LAYER { ENCODER_CCW_CW(_______, _______) }
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] =   BLANK_LAYER,
-    [2] =   BLANK_LAYER
-};
-#endif
