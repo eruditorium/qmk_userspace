51293c8ba3de53c494a0ca4caa723efe171a442a
Nicolas Trapp <n.trapp@du.szmf.de>
2024-04-19 15:15:37 +0200

    debugging

--- keyboards/tominabox1/le_chiffre/keymaps/vial/rules.mk
+++ keyboards/tominabox1/le_chiffre/keymaps/vial/rules.mk
@@ -1,12 +1,6 @@
 VIA_ENABLE = yes
-ENCODER_MAP_ENABLE = no
 VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
 
-LTO_ENABLE = yes
-
-#VIAL_INSECURE = yes # ~374 bytes.
-
 QMK_SETTINGS = yes # ~2560 bytes
-TAP_DANCE_ENABLE = yes # ~2196 bytes
 COMBO_ENABLE = yes # ~2408 bytes
