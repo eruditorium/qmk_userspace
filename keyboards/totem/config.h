099c251caf59b8a569a230184535a3a4a77bf413
yt <y.t@gmx.de>
2024-05-09 12:17:12 +0200

    debugged monorail2, le chiffre and totem

--- keyboards/totem/config.h
+++ keyboards/totem/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*#define VENDOR_ID       0x3A3C
 #define PRODUCT_ID      0x0002
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    GEIST
+#define manufacturer   GEIST
 #define PRODUCT         TOTEM
 */
 
