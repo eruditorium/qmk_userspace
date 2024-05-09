63985260011a013ffe5c4eb0cb4db75505bbeec7
yt <y.t@gmx.de>
2024-05-09 12:34:22 +0200

    revert changes to le chiffre

--- keyboards/tominabox1/le_chiffre/keymaps/eruditorium/rules.mk
+++ keyboards/tominabox1/le_chiffre/keymaps/eruditorium/rules.mk
@@ -1,31 +1,42 @@
-# MCU name
-MCU = RP2040
-BOOTLOADER = rp2040
-
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE = yes       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+OLED_ENABLE = no
+ENCODER_MAP_ENABLE = no
+
+VIAL_ENABLE = no
+QMK_SETTINGS = no
+MOUSEKEY_ENABLE = no        # Mouse keys
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = yes        # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+AUTO_SHIFT = no
 
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = ws2812
-WS2812_DRIVER = vendor
+DYNAMIC_MACRO_ENABLE = no
+COMBO_ENABLE = yes
+EXTRAKEY_ENABLE = yes
 
-API_SYSEX_ENABLE = no
-TAP_DANCE_ENABLE = yes
-ENCODER_ENABLE = yes
+# https://docs.qmk.fm/#/feature_caps_word
+# make sure COMMAND_ENABLE = no
+CAPS_WORD_ENABLE = yes
 
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+#-----------------------------------------------------------------------------
+# getreuer
+#-----------------------------------------------------------------------------
+# https://github.com/getreuer/qmk-keymap
+
+# SRC += features/achordion.c
+# SRC += features/autocorrection.c
+# SRC += features/custom_shift_keys.c
+SRC += features/select_word.c
+
+
+#-----------------------------------------------------------------------------
+# sunaku
+#-----------------------------------------------------------------------------
+
+# https://docs.qmk.fm/#/custom_quantum_functions?id=deferred-execution
+DEFERRED_EXEC_ENABLE = yes
+
