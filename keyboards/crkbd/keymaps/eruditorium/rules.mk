3652f55fbc56edfcf54f98ab06ae4c2907d394ed
Nico <60152863+eruditorium@users.noreply.github.com>
2025-04-29 23:52:27 +0200

    reducing memory footprint

--- keyboards/crkbd/keymaps/eruditorium/rules.mk
+++ keyboards/crkbd/keymaps/eruditorium/rules.mk
@@ -24,7 +24,7 @@ AUTO_SHIFT_ENABLE = no
 
 # https://docs.qmk.fm/#/feature_caps_word
 # make sure COMMAND_ENABLE = no
-CAPS_WORD_ENABLE = yes
+#CAPS_WORD_ENABLE = yes
 
 # https://docs.qmk.fm/#/custom_quantum_functions?id=deferred-execution
 DEFERRED_EXEC_ENABLE = yes
