cd28052ef4ed6116acbeea5f747dfbd7099ffdac
Nico <60152863+eruditorium@users.noreply.github.com>
2025-05-16 21:34:35 +0200

    Update config.h

--- keyboards/crkbd/keymaps/eruditorium/config.h
+++ keyboards/crkbd/keymaps/eruditorium/config.h
@@ -56,9 +56,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger
 
 #ifdef RGBLIGHT_ENABLE
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
